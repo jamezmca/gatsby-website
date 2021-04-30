53ca1a5bb5b913088da428a5fcb40f87d6f20b82
James <jamesmca@live.com>
2021-04-30 13:48:01 -0700

    npm install
    installed all packages asides node sass and env cmd

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,44 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  /*localhost:8000/___graphql*/
+  siteMetadata: {
+    title: 'Swagman Full-Stack Gatsby',
+    author: 'James McArthur'
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    }
+  ],
 }
+
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log('Using environment config: ${activeEnv}')
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
